c3f04e42b389d2d3a4156746df6c50343798b511
skaerf <lawrence.harrison@outlook.com>
2022-10-31 11:13:48 +0000

    moved remote account creation to independent file for ease of use

--- src/main/java/GetHandler.java
+++ src/main/java/GetHandler.java
@@ -19,17 +19,7 @@ public class GetHandler implements HttpHandler {
             requestInfo.put(split[0], split[1]);
         }
         String response = "Request received\n\n\n"+ requestInfo;
-        // TODO EXECUTE REQUESTS
-        if (requestInfo.get("task").equalsIgnoreCase("createAccount")) {
-            // TODO KNOWN IPS SHOULD NOT BE NULL
-            String creationResponse = AccountManager.createNew(new Account(requestInfo.get("username"), requestInfo.get("email"), requestInfo.get("firstname"), requestInfo.get("surname"), null, null));
-            if (creationResponse != null) {
-                response = response + "\n\n\nRESULT="+creationResponse;
-            }
-            else {
-                response = response = "\n\n\nRESULT=successful";
-            }
-        }
+        // TODO EXECUTE REQUESTS NOT RELATED TO ESSENTIALS
         Main.parseQuery(requestedUri.getRawQuery(), parameters);
         he.sendResponseHeaders(200, response.length());
         OutputStream outStream = he.getResponseBody();
