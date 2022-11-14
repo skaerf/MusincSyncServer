d76fcb2b09bd9c50835ba76c68554cfc935f8fbf
skaerf <lawrence.harrison@outlook.com>
2022-11-14 16:53:35 +0000

    add version number to root

--- src/main/java/xyz/skaerf/MusincServer/RootHandler.java
+++ src/main/java/xyz/skaerf/MusincServer/RootHandler.java
@@ -11,7 +11,11 @@ public class RootHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange he) throws IOException {
         System.out.println("Root request received from "+he.getRemoteAddress()+" for "+he.getRequestURI());
-        String response = "<h1>How did you find this then?</h1>";
+        String ver = getClass().getPackage().getImplementationVersion();
+        if (ver == null) {
+            ver = "NON PRODUCTION - DEVELOPMENT BUILD";
+        }
+        String response = "<h1>MusincSyncServer</h1>\n" + "\n" + "<p>Node number: ?</p>\n" + "<p>Server version: " + ver + "</p>";
         he.sendResponseHeaders(200, response.length());
         OutputStream outStream = he.getResponseBody();
         outStream.write(response.getBytes());
