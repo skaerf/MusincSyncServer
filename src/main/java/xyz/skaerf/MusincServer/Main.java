7574d8b5867f7698b93659531021d7d3e8dff272
skaerf <lawrence.harrison@outlook.com>
2022-11-14 22:48:27 +0000

    code cleaning

--- src/main/java/xyz/skaerf/MusincServer/Main.java
+++ src/main/java/xyz/skaerf/MusincServer/Main.java
@@ -91,37 +91,41 @@ public class Main {
         return null;
     }
 
-    public static void parseQuery(String query, HashMap<String, Object> parameters) throws UnsupportedEncodingException {
+    public static void parseQuery(String query, HashMap<String, Object> parameters) {
         if (query != null) {
-            String[] pairs = query.split("[&]");
+            String[] pairs = query.split("&");
             for (String pair : pairs) {
-                String[] param = pair.split("[=]");
+                String[] param = pair.split("=");
                 String key = null;
                 String value = null;
-                if (param.length > 0) {
-                    key = URLDecoder.decode(param[0],
-                            System.getProperty("file.encoding"));
+                try {
+                    if (param.length > 0) {
+                        key = URLDecoder.decode(param[0],
+                                System.getProperty("file.encoding"));
+                    }
+                    if (param.length > 1) {
+                        value = URLDecoder.decode(param[1],
+                                System.getProperty("file.encoding"));
+                    }
                 }
-
-                if (param.length > 1) {
-                    value = URLDecoder.decode(param[1],
-                            System.getProperty("file.encoding"));
+                catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
-
                 if (parameters.containsKey(key)) {
                     Object obj = parameters.get(key);
                     if (obj instanceof List<?>) {
                         List<String> values;
                         values = (List<String>) obj;
                         values.add(value);
-
-                    } else if (obj instanceof String) {
+                    }
+                    else if (obj instanceof String) {
                         List<String> values = new ArrayList<>();
                         values.add((String) obj);
                         values.add(value);
                         parameters.put(key, values);
                     }
-                } else {
+                }
+                else {
                     parameters.put(key, value);
                 }
             }
