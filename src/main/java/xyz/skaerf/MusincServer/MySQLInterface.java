42d9cbd060de06d85fa17d481146b46c45053cd1
skaerf <lawrence.harrison@outlook.com>
2022-11-15 00:18:36 +0000

    code neatening

--- src/main/java/xyz/skaerf/MusincServer/MySQLInterface.java
+++ src/main/java/xyz/skaerf/MusincServer/MySQLInterface.java
@@ -8,20 +8,19 @@ public class MySQLInterface {
 
     private static final String username = "serverTest";
     private static final String password = "muchTestMoment";
-    private static final Statement statement = null;
-    private static Connection connection; //.createStatement()
+    private static Connection connection;
 
     public static void connectDatabase() {
         try {
             if (!Inet4Address.getLocalHost().getHostAddress().equalsIgnoreCase("192.168.56.1")) {
                 connection = DriverManager.getConnection("jdbc:mysql://home.skaerf.xyz:2291/usrs", username, password);
                 System.out.println("Successfully connected to database");
-                //ResultSet resultSet = statement.executeQuery("select * from *");
             }
             else {
                 ErrorHandler.warn("Not going to attempt a connection to database due to MySQL access being blocked by institution network. Notice some features will be unavailable and/or fire errors due to this");
             }
-        } catch (SQLException | UnknownHostException e) {
+        }
+        catch (SQLException | UnknownHostException e) {
             ErrorHandler.fatal("Cannot find a valid driver for JDBC or the connection link is invalid", e.getMessage());
         }
     }
@@ -29,6 +28,7 @@ public class MySQLInterface {
     public static ResultSet executeStatement(String sqlString) {
         ResultSet resSet;
         try {
+            Statement statement = connection.createStatement();
             resSet = statement.executeQuery(sqlString);
             return resSet;
         }
